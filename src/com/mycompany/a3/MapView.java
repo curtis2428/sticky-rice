537a817e76047c7fe2f29c7f54370fc237e91001
curtis2428 <114844835+curtis2428@users.noreply.github.com>
2023-11-27 16:04:33 -0800

    Update MapView.java

--- src/com/mycompany/a3/MapView.java
+++ src/com/mycompany/a3/MapView.java
@@ -11,8 +11,7 @@ import com.codename1.ui.plaf.Border;
 
 public class MapView extends Container implements Observer {
 
-		private GameWorld gw;		
-		
+		private GameWorld gw;				
 		public MapView (GameWorld gw) {
 			
 			this.getAllStyles().setBorder(Border.createLineBorder(5, ColorUtil.rgb(255, 0, 0)));
@@ -30,111 +29,47 @@ public class MapView extends Container implements Observer {
 		}
 		
 		@Override
-		public void paint(Graphics g) {
-			
-			// Call the parent paint method and pass in the graphics object
+		public void paint(Graphics g) {		
 			super.paint(g);
-			
-			// Create a new point using the x and y coordinates of the MapView relative to the screen
 			Point pCmpRelPrnt = new Point(getX(), getY());
-			
-			// Create an iterator to keep track of the objects
 			IIterator iterator = gw.getIterator();
-									
-			// Here we check to see if the iterator that we are using has a next object
 			while(iterator.hasNext()) {
-				
-				// If so then we create a new temporary objects and call getNext() which passes in the next object so we can store it
 				GameObject tempObject = iterator.getNext();
-				
-				// Here we check to see if that object is an instance of the IDrawable object
-				if (tempObject instanceof IDrawable) {
-					
-					// If so then we can call the draw method using the IDrawable object
-					((IDrawable) tempObject).draw(g, pCmpRelPrnt);
-					
-				}
-				
-			}
-			
+				if (tempObject instanceof IDrawable) {					
+					((IDrawable) tempObject).draw(g, pCmpRelPrnt);					
+				}				
+			}			
 		}
 		
-		// This method will be called when a pointer is pressed
 		@Override
-		public void pointerPressed(int xPointer, int yPointer) {		
-			
-			// Check to see if the game is currently paused
-			if(gw.getPaused()) {
-				
-				// Create the x and y coordinates of the point where the user has clicked
+		public void pointerPressed(int xPointer, int yPointer) {					
+			if(gw.getPaused()) {				
 				int xCoordinate = xPointer - getParent().getAbsoluteX();
-				int yCoordinate = yPointer - getParent().getAbsoluteY();
+				int yCoordinate = yPointer - getParent().getAbsoluteY();				
+				Point pPtrRelPrnt = new Point(xCoordinate, yCoordinate);				
+				Point pCmpRelPrnt = new Point(getX(), getY());				
 				
-				// Create a point to keep track where the user has clicked
-				Point pPtrRelPrnt = new Point(xCoordinate, yCoordinate);
-				
-				// Create a point to keep track of the object
-				Point pCmpRelPrnt = new Point(getX(), getY());
-				
-				
-				// Create an iterator to keep track of the objects
-				IIterator iterator = gw.getIterator();
-				
-				// Here we check to see if the iterator that we are using has a next object
-				while(iterator.hasNext()) {
-					
-					// If so then we create a new temporary objects and call getNext() which passes in the next object so we can store it
-					GameObject tempObject = iterator.getNext();
-					
-					// Check to see if the first object was Fixed
-					if(tempObject instanceof Fixed) {
-						
-						// If so then we capture that object as a Fixed object
-						Fixed fixedObject = ((Fixed) tempObject);
-						
-						
-						// Check to see if the fixed item contains the 
-						if(fixedObject.contains(pPtrRelPrnt, pCmpRelPrnt)) {
-							
-							// Select the object
-							fixedObject.setSelected(true);
-							
-							// Update the position variable to be false (we cant move an item)
-							gw.setPosition(false);
-							
-						}
-						
-						// Check to see if the fixed object is selected
-						else if(fixedObject.isSelected()) {
-							
-							// Check to see if we are allowed to change the position of the object
-							if(gw.getPosition() == true) {
-								
-								// Create new x and y coordinates for the existing object relative to the parent container
+				IIterator iterator = gw.getIterator();				
+				while(iterator.hasNext()) {					
+					GameObject tempObject = iterator.getNext();					
+					if(tempObject instanceof Fixed) {						
+						Fixed fixedObject = ((Fixed) tempObject);												
+						if(fixedObject.contains(pPtrRelPrnt, pCmpRelPrnt)) {							
+							fixedObject.setSelected(true);							
+							gw.setPosition(false);							
+						}						
+						else if(fixedObject.isSelected()) {						
+							if(gw.getPosition() == true) {								
 								float xCoordinateNew = pPtrRelPrnt.getX() - pCmpRelPrnt.getX();
-								float yCoordinateNew = pPtrRelPrnt.getY() - pCmpRelPrnt.getY();
-									
-								// Set the location of the fixed object to that of the new one
-								fixedObject.setLocation(xCoordinateNew, yCoordinateNew);
-								
-							}
-							
-							// Update the position variable to be false (we cant move an item)
-							gw.setPosition(false);
-								
-							// Unselect the object
-							fixedObject.setSelected(false);
-							
-						}
-						
-					}
-					
-				}
-				
-				// Update the view on the screen
-				revalidate();
-				
-			}
-			
+								float yCoordinateNew = pPtrRelPrnt.getY() - pCmpRelPrnt.getY();									
+								fixedObject.setLocation(xCoordinateNew, yCoordinateNew);								
+							}							
+							gw.setPosition(false);							
+							fixedObject.setSelected(false);							
+						}						
+					}					
+				}				
+				revalidate();				
+			}			
 		}
 }
