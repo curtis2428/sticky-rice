0d591638b5e2c9aa2d5fad681bbee999c2c3e262
curtis2428 <114844835+curtis2428@users.noreply.github.com>
2023-11-27 16:02:11 -0800

    Update Position.java

--- src/com/mycompany/a3/Position.java
+++ src/com/mycompany/a3/Position.java
@@ -5,29 +5,17 @@ import com.codename1.ui.events.ActionEvent;
 
 public class Position extends Command {
 	
-	// Create a private GameWorld object so we can manipulate the one that is given in the constructor
 	private GameWorld gw;
 
 	
-	// Constructor
-	public Position(GameWorld gw) {
-		
-		// Pass the overridden text to the parent constructor
-		super("Position");
-		
-		// Set the local GameWorld (gw) object to the one that was given in the constructor
+	public Position(GameWorld gw) {		
+		super("Position");		
 		this.gw = gw;
 		
 	}
-
-	// This action will be performed when the command is pushed
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		// Check to see if game is currently paused
-		if (gw.getPaused()) {
-			
-			// Revert the position variable to the opposite of what it currently was
+	public void actionPerformed(ActionEvent e) {		
+		if (gw.getPaused()) {			
 			gw.revertPosition();
 			//gw.getCurrentClock();
 			
